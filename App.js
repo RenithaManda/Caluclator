4cd1840716688aaa8d094360a6910a0142409cae
RenithaManda <98136607+RenithaManda@users.noreply.github.com>
2023-07-31 20:34:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import Calc from './Components/Calc';
+import './Components/Calc.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Calc/>
+
+    </div>
+  );
+}
+
+export default App;
